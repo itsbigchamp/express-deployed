0f6b9cf91486a67cd038c69d9c10448a8f33c62e
Gavin Anderson <btcgrowth1@gmail.com>
2022-08-22 15:35:32 -0400

    added indigo the great dane

--- functions/src/dogs.js
+++ functions/src/dogs.js
@@ -1,7 +1,8 @@
 export function getDogs(req,res) {
     res.send([
         {name: 'Ryder', age: 4, breed: 'dumbass', gender: 'MN'},
-        {name: 'Duke', age: 2, breed: 'Rotweiler', gender: 'M'}
+        {name: 'Duke', age: 2, breed: 'Rotweiler', gender: 'M'},
+        {name: 'Indigo', age: 10, breed: 'Great Dane/ Lab Mix', gender: 'MN'}
     ]);
 }
 
